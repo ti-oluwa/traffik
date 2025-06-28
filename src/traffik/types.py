45056a274d36ba326b31db5140e0d6161f44b6b5
Akindotcome <savichitech@gmail.com>
2025-06-28 20:16:07 +0100

    Refactored throttling exception logic to use UNLIMITED sentinel instead of exception handling. Tests are passing

--- src/traffik/types.py
+++ src/traffik/types.py
@@ -10,6 +10,24 @@ R = typing.TypeVar("R")
 S = typing.TypeVar("S")
 T = typing.TypeVar("T")
 
+
+UNLIMITED = object()
+"""
+A sentinel value to identify that a connection should not be throttled.
+
+This value should be returned by the connection identifier function
+when the connection should not be subject to throttling.
+"""
+
+
+class Stringable(typing.Protocol):
+    """Protocol for objects that can be converted to a string."""
+
+    def __str__(self) -> str:
+        """Return a string representation of the object."""
+        ...
+
+
 Function: TypeAlias = typing.Callable[P, R]
 CoroutineFunction: TypeAlias = typing.Callable[P, typing.Awaitable[R]]
 Decorated: TypeAlias = typing.Union[Function[P, R], CoroutineFunction[P, R]]
@@ -17,7 +35,7 @@ Dependency: TypeAlias = typing.Union[Function[Q, S], CoroutineFunction[Q, S]]
 
 HTTPConnectionT = typing.TypeVar("HTTPConnectionT", bound=HTTPConnection)
 ConnectionIdentifier: TypeAlias = typing.Callable[
-    [HTTPConnectionT], typing.Awaitable[typing.Any]
+    [HTTPConnectionT], typing.Awaitable[typing.Union[Stringable, typing.Any]]
 ]
 
 WaitPeriod: TypeAlias = int

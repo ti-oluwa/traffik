36d59380858b3c9727b0f22c5dc9ce107bbc0224
ti-oluwa <tioluwa.dev@gmail.com>
2026-05-07 23:49:40 +0100

    chore: Replace deprecated asyncio.iscoroutinefunction with inspect version

--- src/traffik/utils.py
+++ src/traffik/utils.py
@@ -142,8 +142,8 @@ def is_async_callable(obj: typing.Any) -> typing.Any:
     while isinstance(obj, functools.partial):
         obj = obj.func
 
-    return asyncio.iscoroutinefunction(obj) or (
-        callable(obj) and asyncio.iscoroutinefunction(obj.__call__)  # type: ignore
+    return inspect.iscoroutinefunction(obj) or (
+        callable(obj) and inspect.iscoroutinefunction(obj.__call__)  # type: ignore
     )
 
 

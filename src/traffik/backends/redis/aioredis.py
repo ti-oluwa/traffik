aa2530f2ffb29543c2d9dc5cbc3cc7f20e429335
ti-oluwa <tioluwa.dev@gmail.com>
2026-08-21 16:29:17 +0100

    fix type checking issue

--- src/traffik/backends/redis/aioredis.py
+++ src/traffik/backends/redis/aioredis.py
@@ -674,12 +674,10 @@ class RedisBackend(ThrottleBackend[_AnyRedis, HTTPConnectionT]):
             nodes = []
             for item in raw:
                 if isinstance(item, dict):
-                    nodes.append(
-                        {
-                            "host": item["host"],
-                            "port": int(item.get("port", 6379)),
-                        }
-                    )
+                    nodes.append({
+                        "host": item["host"],
+                        "port": int(item.get("port", 6379)),
+                    })
                 elif isinstance(item, (list, tuple)) and len(item) == 2:
                     nodes.append({"host": item[0], "port": int(item[1])})
                 else:
@@ -778,7 +776,7 @@ class RedisBackend(ThrottleBackend[_AnyRedis, HTTPConnectionT]):
             return False
 
         try:
-            await self.connection.ping()
+            await self.connection.ping()  # type: ignore[misc]
             return await self._check_scripts_ready()
         except aioredis.RedisError:
             return False
